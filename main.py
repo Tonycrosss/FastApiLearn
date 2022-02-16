346750ba868eba03f8efe2a681cc72ebd0475a82
Mpak <starovoitov.anton@gmail.com>
2022-02-17 00:12:46 +0500

    added some post\get and schemas

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 from fastapi import FastAPI
+from schemas import Book
 
+app = FastAPI()
+
+@app.get('/')
+def home():
+    return {'key': 'Hello'}
+
+
+@app.get('/{pk}')
+def get_item(pk: int, q: str = None):
+    return {'key': pk, 'q': q}
+
+@app.get('/user/{pk}/items/{item}/')
+def get_user_item(pk: int, item: str):
+    return {'user': pk, 'item': item}
+
+@app.post('/book')
+def create_book(item: Book):
+    return item
