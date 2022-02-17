025145476b951f06205f967f462575f2ab25415b
Mpak <starovoitov.anton@gmail.com>
2022-02-18 02:39:44 +0500

    added test_post example

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,18 +1,44 @@
+import json
+
 import pytest
 import httpx
 from typing import List
 
+from schemas import Book
+
 
 @pytest.mark.smoke_test
 @pytest.mark.parametrize('q', [['book1', 'book4'], ['book5', 'book6']])
-def test_get_book(q: List[str]):
-    resp = httpx.get(
-        url='http://127.0.0.1:8080/book',
+def test_get_book(api_client, q: List[str]):
+    res = api_client.get(
+        path='/book',
         params={'q': q}
     )
     expected_result = q
-    fact_result = resp.json()
+    fact_result = res.json()
     print(f"Expected result was {expected_result} an the fact result is {fact_result}")
     assert expected_result == fact_result
 
+test_book_dict = {
+    "title": "Magic",
+    "writer": "string",
+    "duration": "string",
+    "date": "2022-02-17",
+    "summary": "string",
+    "genres": [
+        {
+            "name": "string"
+        }
+    ],
+    "pages": 1
+}
 
+@pytest.mark.parametrize('item', [test_book_dict])
+def test_create_book(api_client, item: Book):
+    res = api_client.post(
+        path='/book',
+        data=json.dumps(item)
+    )
+    expected_result = item
+    actual_result = res.json()
+    assert expected_result == actual_result
